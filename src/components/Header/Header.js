c243b6fd806b0f155a27c5b801c92f6b65f6bdae
akchabekova <Luelum7@gmail.com>
2023-08-17 16:36:30 +0200

    add footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import ImgFilt from "../../assets/img/filtration.png"
 
 
 const HeaderPage = styled.div`
-background-color: #24255B;
+  background-color: #24255B;
 `;
 
 
@@ -19,8 +19,6 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  
- 
 `;
 
 const Left = styled.div`
@@ -29,13 +27,9 @@ const Left = styled.div`
 `;
 
 const Logo = styled.div`
-  img{
-      cursor: pointer;
+  img {
+    cursor: pointer;
   }
-
-
-
-
 `;
 
 const SearchContainer = styled.div`
@@ -44,16 +38,13 @@ const SearchContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   position: relative;
-  img{
+  img {
     content: "";
     position: absolute;
     top: 6px;
     right: 15px;
   }
-  
-  
 
-  
 `;
 
 const Input = styled.input`
@@ -61,34 +52,31 @@ const Input = styled.input`
   padding: 10px;
   box-shadow: 2px 1px 4px 0 #c9c3c3;
   border-radius: 25px;
-   width: 500px;
-  background-color:#24255B;
+  width: 500px;
+  background-color: #24255B;
   color: white;
   position: relative;
-
-
 
 `;
 
 const Menu = styled.div`
-display: flex;
+  display: flex;
   align-items: center;
-
 `;
 const Status = styled.div`
 
 `;
 const UserPage = styled.div`
-display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
- margin-left: 15px;
+  margin-left: 15px;
 `;
 const Registr = styled.div`
   margin-left: 10px;
   cursor: pointer;
   font-size: 18px;
-  
+
 `;
 
 const Logn = styled.div`
@@ -97,45 +85,45 @@ const Logn = styled.div`
 `;
 
 const BoxStatus = styled.div`
-cursor: pointer;
+  cursor: pointer;
   margin-left: 15px;
 `;
 const Filtration = styled.div`
-cursor: pointer;
- margin-left: 15px;
+  cursor: pointer;
+  margin-left: 15px;
 `;
 
 
 const Header = () => {
-        const navigate = useNavigate();
+    const navigate = useNavigate();
     return (
         <HeaderPage>
-        <Container>
-            <Left>
-                <Logo onClick={() => navigate("/home")}>
+            <Container>
+                <Left>
+                    <Logo onClick={() => navigate("/home")}>
                         <img src={ImgLogo} width="100" height="100"/>
-                </Logo>
-            </Left>
-             <Menu>
-                 <SearchContainer>
-                       <Input placeholder="Поиск..." type="text" size="30" color="white" />
-                     <img src={ImgSearch} width="25" height="25"/>
-                 </SearchContainer>
-                 <Filtration>
-                       <img src={ImgFilt} width="25" height="25"/>
-                 </Filtration>
-                 <Status>
-                     <BoxStatus>
-                         <img src={ImgStatus} width="25" height="25"/>
-                     </BoxStatus>
-                 </Status>
-                <UserPage>
-                       <Logn  onClick={() => navigate("/login")}>Login</Logn>
-                    <Registr  onClick={() => navigate("/register")}>Register</Registr>
-                </UserPage>
-            </Menu>
-        </Container>
-            </HeaderPage>
+                    </Logo>
+                </Left>
+                <Menu>
+                    <SearchContainer>
+                        <Input placeholder="Поиск..." type="text" size="30" color="white"/>
+                        <img src={ImgSearch} width="25" height="25"/>
+                    </SearchContainer>
+                    <Filtration>
+                        <img src={ImgFilt} width="25" height="25"/>
+                    </Filtration>
+                    <Status>
+                        <BoxStatus>
+                            <img src={ImgStatus} width="25" height="25"/>
+                        </BoxStatus>
+                    </Status>
+                    <UserPage>
+                        <Logn onClick={() => navigate("/login")}>Login</Logn>
+                        <Registr onClick={() => navigate("/register")}>Register</Registr>
+                    </UserPage>
+                </Menu>
+            </Container>
+        </HeaderPage>
     );
 };
 
