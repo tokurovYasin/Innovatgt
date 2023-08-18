05919b6731be90e2534238647f1b6cbac45a6383
Yasin <yasin4911@gmail.com>
2023-08-18 16:27:10 +0600

    ss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import {useNavigate} from "react-router-dom";
-import ImgLogo from "../../assets/img/logo kitepter.png"
-import ImgStatus from "../../assets/img/Bookmark.png"
-import ImgSearch from "../../assets/img/Search_light.png"
-import ImgFilt from "../../assets/img/filtration.png"
-import ImgNotification from "../../assets/img/Bell_pin.png"
-
+import { useNavigate } from "react-router-dom";
+import ImgLogo from "../../assets/img/logo kitepter.png";
+import ImgStatus from "../../assets/img/Bookmark.png";
+import ImgSearch from "../../assets/img/Search_light.png";
+import ImgFilt from "../../assets/img/filtration.png";
+import ImgNotification from "../../assets/img/Bell_pin.png";
 
 const HeaderPage = styled.div`
-  background-color: #24255B;
+  background-color: #24255b;
 `;
 
 const Container = styled.div`
@@ -35,7 +34,7 @@ const Logo = styled.div`
 `;
 
 const SearchContainer = styled.div`
-  font-family: 'Jura', sans-serif;
+  font-family: "Jura", sans-serif;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -52,7 +51,6 @@ const SearchContainer = styled.div`
     right: 28px;
     top: 4px;
   }
-
 `;
 
 const Input = styled.input`
@@ -63,7 +61,7 @@ const Input = styled.input`
   box-shadow: 2px 1px 4px 0 #c9c3c3;
   border-radius: 25px;
   width: 600px;
-  background-color: #24255B;
+  background-color: #24255b;
 
   position: relative;
 
@@ -72,17 +70,13 @@ const Input = styled.input`
     font-size: 18px;
     padding-left: 30px;
   }
-
-
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
 `;
-const Status = styled.div`
-
-`;
+const Status = styled.div``;
 const UserPage = styled.div`
   display: flex;
   justify-content: center;
@@ -90,16 +84,15 @@ const UserPage = styled.div`
   margin-left: 15px;
 `;
 const Registr = styled.div`
-    margin-left: 18px;
-    cursor: pointer;
-    font-size: 20px;
-
+  margin-left: 18px;
+  cursor: pointer;
+  font-size: 20px;
 `;
 
 const Logn = styled.div`
-   margin-left: 18px;
-    cursor: pointer;
-    font-size: 20px;
+  margin-left: 18px;
+  cursor: pointer;
+  font-size: 20px;
 `;
 
 const BoxStatus = styled.div`
@@ -115,39 +108,38 @@ const Notifiсation = styled.div`
   margin-left: 20px;
 `;
 
-
 const Header = () => {
-    const navigate = useNavigate();
-    return (
-        <HeaderPage>
-            <Container>
-                <Logo onClick={() => navigate("/home")}>
-                    <img src={ImgLogo} width="100" height="100"/>
-                </Logo>
-                <SearchContainer>
-                    <Input placeholder="Поиск..." type="text" size="30" color="white"/>
-                    {/*<img src={ImgSearch} width="30" height="30"/>*/}
-                    <Filtration>
-                        <img src={ImgFilt} width="43" height="43"/>
-                    </Filtration>
-                </SearchContainer>
-                <Menu>
-                    <Status>
-                        <BoxStatus>
-                            <img src={ImgStatus} width="30" height="30"/>
-                        </BoxStatus>
-                    </Status>
-                    <Notifiсation>
-                        <img src={ImgNotification} width="30" height="30"/>
-                    </Notifiсation>
-                    <UserPage>
-                        <Logn onClick={() => navigate("/login")}>Логин</Logn>
-                        <Registr onClick={() => navigate("/register")}>Регистрация</Registr>
-                    </UserPage>
-                </Menu>
-            </Container>
-        </HeaderPage>
-    );
+  const navigate = useNavigate();
+  return (
+    <HeaderPage>
+      <Container>
+        <Logo onClick={() => navigate("/home")}>
+          <img src={ImgLogo} width="100" height="100" />
+        </Logo>
+        <SearchContainer>
+          <Input placeholder="Поиск..." type="text" size="30" color="white" />
+          {/*<img src={ImgSearch} width="30" height="30"/>*/}
+          <Filtration>
+            <img src={ImgFilt} width="43" height="43" />
+          </Filtration>
+        </SearchContainer>
+        <Menu>
+          <Status>
+            <BoxStatus>
+              <img src={ImgStatus} width="30" height="30" />
+            </BoxStatus>
+          </Status>
+          <Notifiсation>
+            <img src={ImgNotification} width="30" height="30" />
+          </Notifiсation>
+          <UserPage>
+            <Logn onClick={() => navigate("/login")}>Логин</Logn>
+            <Registr onClick={() => navigate("/register")}>Регистрация</Registr>
+          </UserPage>
+        </Menu>
+      </Container>
+    </HeaderPage>
+  );
 };
 
 export default Header;
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate, RouterProvider } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AddNewBook from "../pages/AddNewBook";
 import AboutBook from "../components/AboutBook";
 import AuthRootComponent from "../pages/Auth";
 import UserPage from "../pages/UserPage";
-
+import Filterr from "../components/Filterr/Filterr";
 const Routers = () => {
-    return (
-        <Routes >
-            <Route path='/' element={<Navigate to={'/home'}/>}/>
-            <Route path='/home' element={<HomePage/>}/>
-            <Route path='/register' element={<AuthRootComponent/>}/>
-            <Route path='/login' element={<AuthRootComponent/>}/>
-            <Route path='/add-book' element={<AddNewBook/>}/>
-            <Route path='/aboutBook' element={<AboutBook/>}/>
-            <Route path='/userpage' element={<UserPage/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/register" element={<AuthRootComponent />} />
+      <Route path="/login" element={<AuthRootComponent />} />
+      <Route path="/add-book" element={<AddNewBook />} />
+      <Route path="/aboutBook" element={<AboutBook />} />
+      <Route path="/userpage" element={<UserPage />} />
+      <Route path="/filter" element={<Filterr />} />
+    </Routes>
+  );
 };
 
 export default Routers;
